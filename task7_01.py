6956b19fdc0bd45af76ae97bce7aa2a7bdddbd58
LaraMusss <musiny_home@rambler.ru>
2023-02-27 21:30:19 +0500

    Про фразы от Винни

---
+++ task7_01.py
@@ -0,0 +1,7 @@
+_list = [len([i for i in el if i.lower() in "уеёэоаыяию"])\
+     for el in input("Введите фразу: ").split()]
+
+if all([i == _list[0] for i in _list]):
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
