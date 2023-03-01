46f0de0bf864f13de762de3ee6498fefd2681407
LaraMusss <musiny_home@rambler.ru>
2023-03-01 21:58:44 +0500

    добавлен коммент

--- task7_01.py
+++ task7_01.py
@@ -4,4 +4,13 @@ _list = [len([i for i in el if i.lower() in "уеёэоаыяию"])\
 if all([i == _list[0] for i in _list]):
     print("Парам пам-пам")
 else:
-    print("Пам парам")
+   print("Пам парам")
+#   def print_operation_table(operation, num_rows=6, num_columns=6):
+#   for i in range(1, num_rows + 1):
+#       a = []
+#       for j in range(1, num_columns + 1):
+#           a.append(str(operation(i, j)))
+#       print(''.join(f'{e:<4}' for e in a))
+#
+#print_operation_table(lambda x, y: x * y)
+# таблица, почемуто не залилась на гитхаб, повтор
--- task7_02.py
+++ task7_02.py
@@ -6,3 +6,4 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
         print(''.join(f'{e:<4}' for e in a))
 
 print_operation_table(lambda x, y: x * y)
+# таблица, почемуто не залилась на гитхаб, повтор
